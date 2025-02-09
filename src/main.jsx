164bb3e9c2448ee927bfba43a302f5fa96c2924b
IkMba <mariahchester89@gmail.com>
2025-02-09 19:09:45 +0100

    fixed persistgate

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 // import { Toaster } from "@/components/ui/toaster"
 
-
-
 import AppRoutes from "./AppRoutes.jsx";
-import  { persistor,store } from "./utils/store.js";
+import { persistor, store } from "./utils/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 // import { PersistGate } from "redux-persist/";
 import { QueryClient, QueryClientProvider } from "react-query";
-
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,20 +20,18 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <QueryClientProvider client={queryClient}>
-        <AppRoutes />
-        <Toaster visibleToasts={1} position="top-right" richColors />
-        {/* <Toaster  /> */}
-
-        </QueryClientProvider>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <QueryClientProvider client={queryClient}>
+            <AppRoutes />
+            <Toaster visibleToasts={1} position="top-right" richColors />
+            {/* <Toaster  /> */}
+          </QueryClientProvider>
+        </Router>
       </PersistGate>
-
     </Provider>
   </StrictMode>
 );
